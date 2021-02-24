ea5f5e72bf0c30233fa7f07b8e72b684e9f6b463
Gabriel Loureiro <loureirobiel@gmail.com>
2021-02-24 20:48:26 -0300

    working with nullable lists

--- src/main/kotlin/list/Livro.kt
+++ src/main/kotlin/list/Livro.kt
@@ -1,11 +1,11 @@
 package list
 
-data class Livro(
+data class Livro (
     val titulo: String,
     val autor: String,
     val anoPublicacao: Long,
     val editora: String? = null
-) : Comparable<Livro> {
+): Comparable<Livro> {
     override fun compareTo(other: Livro): Int {
         return this.anoPublicacao.compareTo(other.anoPublicacao)
     }

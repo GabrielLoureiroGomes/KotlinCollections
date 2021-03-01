0ec8edac345e95b614e42884b27a465861e6cb59
Gabriel Loureiro <loureirobiel@gmail.com>
2021-03-01 19:32:08 -0300

    working with list, mutableList and set

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,23 @@
+fun main() {
+    val assistiramCursoAndroid: Set<String> = setOf("Gabriel", "Jessica", "Fred", "Gustavo")
+    val assistiramCursoKotlin: Set<String> = setOf("Gabriel", "Raimundo", "Maria")
+    val assistiramAmbos = mutableSetOf<String>()
+    assistiramAmbos.addAll(assistiramCursoAndroid)
+    assistiramAmbos.addAll(assistiramCursoKotlin)
+    assistiramAmbos.add("Ana")
+    assistiramAmbos.add("Ana")
+    println(assistiramAmbos)
+
+    println(assistiramCursoKotlin + assistiramCursoAndroid)
+    println(assistiramCursoKotlin union assistiramCursoAndroid)
+
+    println(assistiramCursoAndroid - assistiramCursoKotlin)
+    println(assistiramCursoAndroid subtract assistiramCursoKotlin)
+
+    println(assistiramCursoAndroid intersect assistiramCursoKotlin)
+
+    val assistiramList = assistiramAmbos.toMutableList()
+    assistiramList.add("Gabriel")
+    println(assistiramList)
+    println(assistiramList.toSet())
+}
